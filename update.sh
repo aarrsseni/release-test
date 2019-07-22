ba40393cb067d0c21e6c04d23402744e5b57456e
Arseni Bulatski <ancarseni@gmail.com>
2019-07-22 11:49:45 +0300

    Test

--- update.sh
+++ update.sh
@@ -4,6 +4,7 @@ mkdir temp-homebrew
 cd temp-homebrew
 git clone https://github.com/aarrsseni/homebrew-test-release
 cd homebrew-test-release
+git remote add origin-deploy https://${GITHUB_TOKEN}@github.com/aarrsseni/homebrew-test-release.git git
 cd Formula
 PREV_NAME=$(grep -o 'file_path=.*$' bq.rb | cut -c11-)
 PREV_NAME=${PREV_NAME%?}
@@ -12,4 +13,4 @@ sed -i '.bak' "s/$PREV_NAME/$NAME/g" bq.rb
 cat bq.rb
 git add .
 git commit -m "Update formula version"
-git push origin master
+git push origin-deploy master
