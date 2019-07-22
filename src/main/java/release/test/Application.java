7708de4c0288c9984e47e2288f7472687f3aee05
Arseni Bulatski <ancarseni@gmail.com>
2019-07-22 14:35:44 +0300

    Test graalvm on travis

--- src/main/java/release/test/Application.java
+++ src/main/java/release/test/Application.java
@@ -1,12 +1,8 @@
 package release.test;
 
-import io.bootique.Bootique;
-
 public class Application {
 
     public static void main(String[] args) {
-        Bootique
-                .app(args)
-                .exec();
+
     }
 }
