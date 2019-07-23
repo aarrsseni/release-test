1f3ec0aa5ae71f960add81ff5418b6498e5816cc
Arseni Bulatski <ancarseni@gmail.com>
2019-07-23 17:52:31 +0300

    Experiment

--- prepare-linux.sh
+++ prepare-linux.sh
@@ -1,12 +1,16 @@
-sudo apt-get install ruby ruby-dev rubygems build-essential
+#sudo apt-get install ruby ruby-dev rubygems build-essential
+#
+#sudo apt-get install rpmbuild
+#
+#gem install --no-ri --no-rdoc fpm
+#fpm --version
+#
+#mvn clean install -P assembly
+#FILE_ZIP=$(find . -type f -name 'release-test-*.zip')
+#echo ${FILE_ZIP}
+#
+#fpm -s zip -t rpm -n rpmbuild ${FILE_ZIP}
 
-sudo apt-get install rpmbuild
-
-gem install --no-ri --no-rdoc fpm
-fpm --version
-
-mvn clean install -P assembly
-FILE_ZIP=$(find . -type f -name 'release-test-*.zip')
-echo ${FILE_ZIP}
-
-fpm -s zip -t rpm ${FILE_ZIP}
+sudo apt-get install -f ./test_1.0_amd64.deb
+dpkg -s test |  grep Status
+./test
