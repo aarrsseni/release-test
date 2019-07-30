546ab71566c3ba35789a4b739479906490b21f0b
Arseni Bulatski <ancarseni@gmail.com>
2019-07-30 15:11:35 +0300

    Experiment windows

--- prepare-win.sh
+++ prepare-win.sh
@@ -1,27 +1,49 @@
 #!/usr/bin/env bash
 
-choco install graalvm
+#choco install graalvm
 
 # TODO: can this be done by chocolatey? or something else more robust?
-export JAVA_HOME="/c/Program Files/GraalVM/graalvm-ce-19.1.0"
+#export JAVA_HOME="/c/Program Files/GraalVM/graalvm-ce-19.1.0"
 
 # check
-"${JAVA_HOME}/bin/java" -version
+#"${JAVA_HOME}/bin/java" -version
 #./mvnw -v
 #./mvnw clean install
 
-#NAME=$(basename $(find . -type f -name 'release-test-*.jar'))
+#NAME=$(find . -type f -name 'release-test-*.jar')
+#echo ${NAME}
 
 #WORK_DIR=$(pwd)
-#echo ${PWD}
+#echo ${WORK_DIR}
 
-
-#choco uninstall vcredist140 --force --forcedependencies
+choco uninstall KB2919355 --force-dependencies
+choco uninstall KB2919442 --force-dependencies
+choco uninstall KB2999226 --force-dependencies
+choco uninstall KB3033929 --force-dependencies
+choco uninstall KB3035131 --force-dependencies
+choco uninstall microsoft-build-tools --force-dependencies
+choco uninstall vcredist140 --force-dependencies
+choco uninstall vcredist2017 --force-dependencies
+choco uninstall visualstudio2017buildtools --force-dependencies
+choco uninstall visualstudio2017-installer --force-dependencies
+choco uninstall visualstudio2017-workload-netcorebuildtools --force-dependencies
+choco uninstall visualstudio2017-workload-vctools --force-dependencies
+choco uninstall visualstudio2017-workload-webbuildtools --force-dependencies
 
 choco install windows-sdk-7.1
 
-cd "/c/Program Files/Microsoft SDKs/Windows/v7.1/Bin"
 
-ls
+#cd "/c/Program Files (x86)/Microsoft Visual Studio 14.0/VC/bin"
+#ls
 
-./SetEnv
+#./make-shell.exe ${JAVA_HOME}/bin/native-image.cmd -jar "${WORK_DIR}/target/release-test-1.22-SNAPSHOT.jar"
+
+#cd ${WORK_DIR}
+
+./setenv.cmd
+
+#ls
+
+cd "${JAVA_HOME}/bin"
+
+./native-image.cmd -jar "${WORK_DIR}/target/release-test-1.22-SNAPSHOT.jar"
