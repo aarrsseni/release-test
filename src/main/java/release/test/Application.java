85646abb11530866c78762070d321e8d0375c1f0
Arseni Bulatski <ancarseni@gmail.com>
2019-08-02 17:01:14 +0300

    Update

--- src/main/java/release/test/Application.java
+++ src/main/java/release/test/Application.java
@@ -3,6 +3,6 @@ package release.test;
 public class Application {
 
     public static void main(String[] args) {
-        System.out.println("This is new version!");
+        System.out.println("New");
     }
 }
