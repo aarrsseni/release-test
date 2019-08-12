38847329ce20f6a86edc2d6f56364fc594e23ed6
Arseni Bulatski <ancarseni@gmail.com>
2019-08-12 10:00:22 +0300

    Test

--- update-version.sh
+++ update-version.sh
@@ -5,10 +5,10 @@ cd target
 NAME=$(basename $(find . -type f -name 'release-test-*.jar'))
 VERSION=$(echo "${NAME%.*}" | cut -d'-' -f 3)
 
-cd ..
+cd ../
 
-sed -i '.bak' "s/template_version/$VERSION/g" deploy-deb.json
-sed -i '.bak' "s/template_tag/$VERSION/g" deploy-deb.json
+sed -i "s/template_version/$VERSION/g" deploy-deb.json
+sed -i "s/template_tag/$VERSION/g" deploy-deb.json
 
-sed -i '.bak' "s/template_version/$VERSION/g" deploy-rpm.json
-sed -i '.bak' "s/template_tag/$VERSION/g" deploy-rpm.json
+sed -i "s/template_version/$VERSION/g" deploy-rpm.json
+sed -i "s/template_tag/$VERSION/g" deploy-rpm.json
